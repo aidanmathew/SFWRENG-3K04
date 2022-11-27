1b2802553b43ef4aaac07e4b8e7929562d2cf83d
Aidan Mathew <mataid@icloud.com>
2022-11-27 17:49:28 -0500

    Serial Communication Update

--- serialCom.py
+++ serialCom.py
@@ -1,8 +1,3 @@
-"""For Aidan's computer the port was found to be COM4
-frdm_port = serial.Serial('COM4')
-print(frdm_port)
-print(frdm_port.port)
-print()"""
 
 import serial
 import serial.tools.list_ports
@@ -12,7 +7,7 @@ import json
 import sys
 
 
-COMPORT = 'COM3' #change me to the correct COM port
+COMPORT = 'COM4' #change me to the correct COM port
 Start = b'\x16'
 SYNC = b'\x22'
 Fn_set = b'\x55'
@@ -36,7 +31,58 @@ with open(userFilepath+patientDataFile, "r") as iFile:
     # loading the user file date
     patientData = json.load(iFile)
 
-    if (endian_checker == True):
+    if (patientData["pacingMode"] == "AOO"):
+        mode = struct.pack("B",1)
+    elif (patientData["pacingMode"] == "VOO"):
+        mode = struct.pack("B",2)
+    elif (patientData["pacingMode"] == "AAI"):
+        mode = struct.pack("B",3)
+    elif (patientData["pacingMode"] == "VVI"):
+        mode = struct.pack("B",4)
+    elif (patientData["pacingMode"] == "AOOR"):
+        mode = struct.pack("B",5)
+    elif (patientData["pacingMode"] == "VOOR"):
+        mode = struct.pack("B",6)
+    elif (patientData["pacingMode"] == "AAIR"):
+        mode = struct.pack("B",7)
+    elif (patientData["pacingMode"] == "VVIR"):
+        mode = struct.pack("B",8)
+
+    lrl = struct.pack("B",int(patientData["value1"]))
+    url = struct.pack("B",int(patientData["value2"]))
+    msr = struct.pack("B",int(patientData["value3"]))
+    aamp = struct.pack("f",float(patientData["value4"]))
+    apw = struct.pack("B",int(float(patientData["value5"])*10))
+    vamp = struct.pack("f",float(patientData["value6"]))
+    vpw = struct.pack("B",int(float(patientData["value7"])*10))
+    vrp = struct.pack("H",int(patientData["value8"]))
+    arp = struct.pack("H",int(patientData["value9"]))
+    pvarp = struct.pack("B",int(patientData["value10"]))
+    hys = struct.pack("B",int(patientData["value11"]))
+    rs = struct.pack("B",int(patientData["value12"]))
+
+    if ((patientData["value13"]) == "V-Low"):
+        at = struct.pack("f",1.0) #Very Low activity
+    elif (((patientData["value13"]) == "Low")):
+        at = struct.pack("f",2.0) #Low Activity
+    elif ((patientData["value13"]) == "Med-Low"):
+        at = struct.pack("f",3.0) #Med-Low Activity
+    elif ((patientData["value13"]) == "Med"):
+        at = struct.pack("f",4.0) #Med Activity
+    elif ((patientData["value13"]) == "Med-High"):
+        at = struct.pack("f",5.0) #Med-High Activity
+    elif ((patientData["value13"]) == "High"):
+        at = struct.pack("f",6.0) #High Activity
+    elif ((patientData["value13"]) == "V-High"):
+        at = struct.pack("f",7.0) #Very High Activity
+
+    rt = struct.pack("B",int(patientData["value14"]))
+    res = struct.pack("B",int(patientData["value15"]))
+    rct = struct.pack("B",int(patientData["value16"]))
+    asen = struct.pack("f",float(patientData["value17"]))
+    vsen = struct.pack("f",float(patientData["value18"]))
+
+    """if (endian_checker == True):
         if (patientData["pacingMode"] == "AOO"):
             mode = struct.pack("<B",1)
         elif (patientData["pacingMode"] == "VOO"):
@@ -59,28 +105,28 @@ with open(userFilepath+patientDataFile, "r") as iFile:
         msr = struct.pack("<B",int(patientData["value3"]))
         aamp = struct.pack("<f",float(patientData["value4"]))
         apw = struct.pack("<B",int(float(patientData["value5"])*10))
-        vamp = struct.pack("<f",float(patientData["value6"]))
-        vpw = struct.pack("<H",int(float(patientData["value7"])*10))
+        vamp = struct.pack("f",float(patientData["value6"]))
+        vpw = struct.pack("<B",int(float(patientData["value7"])*10))
         vrp = struct.pack("<H",int(patientData["value8"]))
-        arp = struct.pack("<B",int(patientData["value9"]))
+        arp = struct.pack("<H",int(patientData["value9"]))
         pvarp = struct.pack("<B",int(patientData["value10"]))
         hys = struct.pack("<B",int(patientData["value11"]))
         rs = struct.pack("<B",int(patientData["value12"]))
 
         if ((patientData["value13"]) == "V-Low"):
-            at = struct.pack("<f",float(1)) #Very Low activity
+            at = struct.pack("<f",1.0) #Very Low activity
         elif (((patientData["value13"]) == "Low")):
-            at = struct.pack("<f",float(2)) #Low Activity
+            at = struct.pack("<f",2.0) #Low Activity
         elif ((patientData["value13"]) == "Med-Low"):
-            at = struct.pack("<f",float(3)) #Med-Low Activity
+            at = struct.pack("<f",3.0) #Med-Low Activity
         elif ((patientData["value13"]) == "Med"):
-            at = struct.pack("<f",float(4)) #Med Activity
+            at = struct.pack("<f",4.0) #Med Activity
         elif ((patientData["value13"]) == "Med-High"):
-            at = struct.pack("<f",float(5)) #Med-High Activity
+            at = struct.pack("<f",5.0) #Med-High Activity
         elif ((patientData["value13"]) == "High"):
-            at = struct.pack("<f",float(6)) #High Activity
+            at = struct.pack("<f",6.0) #High Activity
         elif ((patientData["value13"]) == "V-High"):
-            at = struct.pack("<f",float(7)) #Very High Activity
+            at = struct.pack("<f",7.0) #Very High Activity
 
         rt = struct.pack("<B",int(patientData["value14"]))
         res = struct.pack("<B",int(patientData["value15"]))
@@ -110,7 +156,7 @@ with open(userFilepath+patientDataFile, "r") as iFile:
         msr = struct.pack(">B",int(patientData["value3"]))
         aamp = struct.pack(">f",float(patientData["value4"]))
         apw = struct.pack(">B",int(float(patientData["value5"])*10))
-        vamp = struct.pack(">f",float(patientData["value6"]))
+        vamp = struct.pack("f",float(patientData["value6"]))
         vpw = struct.pack(">B",int(float(patientData["value7"])*10))
         vrp = struct.pack(">H",int(patientData["value8"]))
         arp = struct.pack(">H",int(patientData["value9"]))
@@ -119,44 +165,72 @@ with open(userFilepath+patientDataFile, "r") as iFile:
         rs = struct.pack(">B",int(patientData["value12"]))
 
         if ((patientData["value13"]) == "V-Low"):
-            at = struct.pack(">f",float(1)) #Very Low activity
+            at = struct.pack(">f",1.0) #Very Low activity
         elif (((patientData["value13"]) == "Low")):
-            at = struct.pack(">f",float(2)) #Low Activity
+            at = struct.pack(">f",2.0) #Low Activity
         elif ((patientData["value13"]) == "Med-Low"):
-            at = struct.pack(">f",float(3)) #Med-Low Activity
+            at = struct.pack(">f",3.0) #Med-Low Activity
         elif ((patientData["value13"]) == "Med"):
-            at = struct.pack(">f",float(4)) #Med Activity
+            at = struct.pack(">f",4.0) #Med Activity
         elif ((patientData["value13"]) == "Med-High"):
-            at = struct.pack(">f",float(5)) #Med-High Activity
+            at = struct.pack(">f",5.0) #Med-High Activity
         elif ((patientData["value13"]) == "High"):
-            at = struct.pack(">f",float(6)) #High Activity
+            at = struct.pack(">f",6.0) #High Activity
         elif ((patientData["value13"]) == "V-High"):
-            at = struct.pack(">f",float(7)) #Very High Activity
+            at = struct.pack(">f",7.0) #Very High Activity
 
         rt = struct.pack(">B",int(patientData["value14"]))
         res = struct.pack(">B",int(patientData["value15"]))
         rct = struct.pack(">B",int(patientData["value16"]))
         asen = struct.pack(">f",float(patientData["value17"]))
-        vsen = struct.pack(">f",float(patientData["value18"]))
+        vsen = struct.pack(">f",float(patientData["value18"]))"""
 
 #print (type(Start),type(Fn_set),type(lrl), type(url), type(vamp), type(aamp), type(vpw), type(apw), type(vsen), type(asen), type(arp), type(vrp), type(msr), type(at), type(rt), type(res), type(rct))
 
-"""print("Mode = " + str(struct.unpack("B",mode)[0]))
-print("Low Rate Limit = " + str(lrl[0]))
-print("Upper Rate Limit = " + str(url[0]))
-print("V-Amp = " + str(vamp[0]))
-print("A-Amp = " + str(aamp[0]))
-print("VPW = " + str(vpw[0]))
-print("APW = " + str(apw[0]))
-print("V-Sen = " + str(vsen[0]))
-print("A-Sen = " + str(asen[0]))
-print("ARP = " + str(arp[0]))
-print("VRP = " + str(vrp[0]))
-print("MSR = " + str(msr[0]))
-print("Activty Threshold = " + str(at[0]))
-print("Reaction Time = " + str(rt[0]))
-print("Response Time = " + str(res[0]))
-print("Recovery Time = " + str(rct[0]))"""
+"""print(int(patientData["value1"]))
+print(int(patientData["value2"]))
+print(int(patientData["value3"]))
+print(float(patientData["value4"]))
+print(int(float(patientData["value5"])*10))
+print(float(patientData["value6"]))
+print(int(float(patientData["value7"])*10))
+print(int(patientData["value8"]))
+print(int(patientData["value9"]))
+print(int(patientData["value10"]))
+print(int(patientData["value11"]))
+print(int(patientData["value12"]))
+print(patientData["value13"])
+print(int(patientData["value14"]))
+print(int(patientData["value15"]))
+print(int(patientData["value16"]))
+print(float(patientData["value17"]))
+print(float(patientData["value18"]))"""
+print("______________________")
+
+
+print("Mode = ", mode[0])
+print("Low Rate Limit = ", lrl[0])
+print("Upper Rate Limit = ", url[0])
+vamp1 = struct.unpack("f", vamp)
+print("V-Amp = " , round(vamp1[0],1))
+aamp1 = struct.unpack("f", aamp)
+print("A-Amp = ", round(aamp1[0],1))
+print("VPW = ", vpw[0])
+print("APW = ", apw[0])
+vsen1 = struct.unpack("f",vsen)
+print("V-Sen = ", vsen1[0])
+asen1 = struct.unpack("f",asen)
+print("A-Sen = ", asen1[0])
+arp1 = struct.unpack("H",arp)
+print("ARP = ", arp1[0])
+vrp1 = struct.unpack("H",vrp)
+print("VRP = ", vrp1[0])
+print("MSR = " , msr[0])
+at1 = struct.unpack("f",at)
+print("Activty Threshold = ", at1[0])
+print("Reaction Time = ", rt[0])
+print("Response Time = ", res[0])
+print("Recovery Time = ", rct[0])
 
 
 Signal_set = Start+Fn_set+mode+lrl+url+vamp+aamp+vpw+apw+vsen+asen+arp+vrp+msr+at+res+rt+rct 
@@ -174,29 +248,44 @@ print("From the board:")
 with serial.Serial(COMPORT, 115200) as pacemaker:
     
     pacemaker.write(Signal_echo)
-
     data = pacemaker.read(33)
-
-    print("HI")
 
     mode2 = data[0]
     lrl2 = data[1]
     url2 = data[2]
-    vamp2 = struct.unpack("f",data[3:7])
-    aamp2 = struct.unpack("f",data[7:11])
+    vamp2 = struct.unpack("f",data[3:7])[0]
+    aamp2 = struct.unpack("f",data[7:11])[0]
     vpw2 = data[11]
     apw2 = data[12]
-    vsen2 = struct.unpack("f",data[13:17])
-    asen2 = struct.unpack("f",data[17:21])
-    arp2 = struct.unpack("H",data[21:23])
-    vrp2 = struct.unpack("H",data[23:25])
+    vsen2 = struct.unpack("f",data[13:17])[0]
+    asen2 = struct.unpack("f",data[17:21])[0]
+    arp2 = struct.unpack("H",data[21:23])[0]
+    vrp2 = struct.unpack("H",data[23:25])[0]
     msr2 = data[25]
-    at2 = struct.unpack("f",data[26:30])
+    at2 = struct.unpack("f",data[26:30])[0]
     res2 = data[30]
     rt2 = data[31]
     rct2 = data[32]
 
-    #print(mode2[0])
+print(data[0])
+
+print("Mode = ", str(mode2))
+print("Low Rate Limit = ", str(lrl2))
+print("Upper Rate Limit = ", str(url2))
+print("V-Amp = ", str(vamp2))
+print("A-Amp = ", str(aamp2))
+print("VPW = ", str(vpw2))
+print("APW = ", str(apw2))
+print("V-Sen = ", str(vsen2))
+print("A-Sen = ", str(asen2))
+print("ARP = ", str(arp2))
+print("VRP = ", str(vrp2))
+print("MSR = ", str(msr2))
+print("Activty Threshold = ", str(at2))
+print("Reaction Time = ", str(rt2))
+print("Response Time = ", str(res2))
+print("Recovery Time = ", str(rct2))
+
 
 
 
